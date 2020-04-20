fe756302514b2b8c4b6062f382f94ced9e6f6ed6
PhilippStaedter <philippstaedter@kabelmail.de>
2020-04-20 19:31:14 +0200

    all python scripts for the study

---
+++ Python_Scripts/JWS_changeValues.py
@@ -0,0 +1,28 @@
+# script to cahange all values for JWS simulation
+
+
+
+def JWS_changeValues(iFile, sedml_file):
+
+    # get all changes from sedml file
+    for iModels in range(0, sedml_file.getNumModels()):
+        all_models = sedml_file.getModel(iModels)
+        model_Id = all_models.getId()
+
+        # take only the current sbml
+        list_of_strings = []
+        if model_Id == iFile:
+            for iAttribute in range(0, all_models.getNumChanges()):
+                all_changes = all_models.getChange(iAttribute)
+                new_targ = all_changes.getTarget()
+                new_val = all_changes.getNewValue()
+
+                # parse whole id out of new_targ string
+                id = new_targ.split('\'', )[1]
+
+                # append information into list_of_strings
+                list_of_strings.append(id + '=' + new_val +';')
+
+
+
+    return list_of_strings
