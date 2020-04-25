694cf1c87c7d8d7ecab8594298e8105888f1ec09
PhilippStaedter <philippstaedter@kabelmail.de>
2020-04-25 22:02:40 +0200

    all python scripts for the study

---
+++ Python_Scripts/execute_WholeStudy.py
@@ -0,0 +1,32 @@
+# execute simulation script with all solver setiing combinations
+
+######################################################################################
+# very important: linSol, nonLinSolIter, solAlg must be their corresponding integers #
+#                                                                                    #
+# linSol:           Dense == 1  GMRES == 6  BiCGStab == 7   SPTFQMR == 8    KLU == 9 #
+# nonLinSolIter:    Functional == 1     Newton-type == 2                             #
+# solAlg:           Adams == 1      BDF == 2                                         #
+#                                                                                    #
+######################################################################################
+
+from SimAllSettings import *
+from tqdm import trange
+
+# settings
+atol = [1e-8, 1e-6, 1e-12, 1e-10, 1e-14, 1e-16, 1e-8]
+rtol = [1e-6, 1e-8, 1e-10, 1e-12, 1e-14, 1e-8, 1e-16]
+linSol = [1, 6, 7, 8, 9]
+iter = [1, 2]
+solAlg = [1, 2]
+maxStep = 10000
+study_indicator = 1                                         # For the main study
+
+
+# run simulation
+for iSolALg in trange(0, len(solAlg)):
+    for iNonLin in trange(0, len(iter)):
+        for iLinSol in trange(0, len(linSol)):
+            for iToleranceTupel in trange(0, len(atol)):
+                simulate(atol[iToleranceTupel], rtol[iToleranceTupel], linSol[iLinSol], iter[iNonLin], solAlg[iSolALg], maxStep, study_indicator)
+                useless_variable = os.system('clear')
+                pass
