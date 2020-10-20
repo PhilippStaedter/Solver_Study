abe557e4efce0106f8a7a9638b9a11823d09cf63
yannikschaelte <yannik.schaelte@gmail.com>
2020-10-20 23:30:45 +0200

    make plotting code more readable

---
+++ Python_Scripts/C.py
@@ -0,0 +1,16 @@
+# The output of the study has been conserved in `BASE_DIR`.
+DIR_BASE = '../'
+# The scripts write the study output in the below file. To directly use that
+#  output, (un-)comment the two lines.
+# DIR_BASE = '../../Benchmarking_of_numerical_ODE_integration_methods/'
+
+# Data directories
+DIR_DATA = DIR_BASE + 'Data/'
+DIR_DATA_WHOLESTUDY = DIR_DATA + 'WholeStudy/'
+DIR_DATA_TOLERANCES = DIR_DATA + 'TolerancesStudy/'
+
+# Model directory
+DIR_MODELS = DIR_BASE + 'Models/'
+
+# JSON files
+DIR_JSONFILES = DIR_BASE + 'json_files/'
